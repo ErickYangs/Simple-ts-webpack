757b5daf4dd3b7cf18ed2d9e11bbd9ceb2d4e17d
ericyang <ericyangchen@foxmail.com>
2020-03-24 14:17:41 +0800

    coes

---
+++ src/___tests___/index.spec.ts
@@ -0,0 +1,14 @@
+import { add } from "../index";
+// import { client } from 'ontology-dapi';
+// client.registerClient({});
+import { Wallet, Identity, Crypto } from 'ontology-ts-sdk'
+test("Index add fun", () => {
+    const ret = add(1, 2)
+    console.log(ret)
+    // expect(ret).toBe(3);
+    // var wallet = Wallet.create('test');
+    // console.log('wallet', wallet)
+    const privateKey = Crypto.PrivateKey.random();
+    var identity = Identity.create(privateKey, '', '')
+    console.log(identity.ontid)
+});
